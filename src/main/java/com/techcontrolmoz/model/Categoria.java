d644ca5e32fa9608a8f65779fc9a404ec4f8e3e8
carlos nhantumbo <nhantumbo@techcontrolmoz.com>
2023-03-30 22:12:23 +0200

    finalizando as classes model

--- src/main/java/com/techcontrolmoz/model/Categoria.java
+++ src/main/java/com/techcontrolmoz/model/Categoria.java
@@ -1,18 +1,27 @@
 package com.techcontrolmoz.model;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Categoria {
-
+@Entity
+public class Categoria  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros =new ArrayList<>();
 
     
--- src/main/java/com/techcontrolmoz/model/Livro.java
+++ src/main/java/com/techcontrolmoz/model/Livro.java
@@ -1,15 +1,26 @@
 package com.techcontrolmoz.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
+    @ManyToOne
+    @JoinColumn(name="categoria_id")
     private Categoria categoria;
 
 }
